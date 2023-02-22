8e9d0ea83881cff753d3d86755e6b7359d5d20a6
Akars <williamli973@gmail.com>
2023-02-22 17:56:55 +0100

    create login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './Login.css'
+
+const Login = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <div className={"login-container"}>
+      <div className={"login-header"}>
+        <img id={"header-logo"} src={"https://auth.myefrei.fr/static/media/logo-efrei.65d4f0ab.png"} alt={"Efrei"}/>
+        <div id={"header-title"}>
+          <h1>Authentication</h1>
+        </div>
+      </div>
+      <form className={"login-form"}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
