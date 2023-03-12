c6583e5e1532f5668a258b2278aabf64515d3d4f
martin <martin.lenaerts@hotmail.fr>
2023-03-12 22:07:55 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Login } from './components/Login';
-import { InternsTable } from './components/InternsTable';
+import {Login} from './components';
+import {InternsTable} from './components/InternsTable';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 export const App = () => {
-  return <InternsTable />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<InternsTable/>}/>
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
